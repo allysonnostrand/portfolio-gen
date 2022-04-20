375cfff865dca67f5170a2b986eca79ca541f9e3
iggy <allyson.nostrandd@gmail.com>
2022-04-20 11:37:47 -0700

    answers displayed in html!

--- index.js
+++ index.js
@@ -36,7 +36,26 @@ inquirer
   ])
   .then((answers) => {
     console.log(answers)
-    fs.writeFile('test.html',JSON.stringify(answers),(err,data)=>{
+    let content = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>${answers.userName}</h1>
+    <h2>${answers.location}</h2>
+    <h3>${answers.bio}</h3>
+    <h4>${answers.linkedIn}</h4>
+    <h5>${answers.gitHub}</h5>
+    
+</body>
+</html>`
+
+    fs.writeFile('portfolio.html',(content),(err,data)=>{
         if(err){
             throw err
         }
